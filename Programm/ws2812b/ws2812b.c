4aea4d51b58779035da9339a767bcef37b6a612d
Sivokon Dmitriy <41055140+DiMoonElec@users.noreply.github.com>
2018-07-20 16:53:57 +0300

    Update ws2812b.c

--- Programm/ws2812b/ws2812b.c
+++ Programm/ws2812b/ws2812b.c
@@ -134,33 +134,33 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #elif (WS2812B_OUTPUT_PAx==3)
 
-#define GPIO_CRL_CNFx           GPIO_CRL_CNF3
-#define GPIO_CRL_CNFx_1         GPIO_CRL_CNF3_1
-#define GPIO_CRL_MODEx_1        GPIO_CRL_MODE3_1
-#define GPIO_CRL_MODEx_0        GPIO_CRL_MODE3_0
+  #define GPIO_CRL_CNFx           GPIO_CRL_CNF3
+  #define GPIO_CRL_CNFx_1         GPIO_CRL_CNF3_1
+  #define GPIO_CRL_MODEx_1        GPIO_CRL_MODE3_1
+  #define GPIO_CRL_MODEx_0        GPIO_CRL_MODE3_0
 
-#define TIM_CCER_CCxE           TIM_CCER_CC4E
-#define TIM_CCER_CCxP           TIM_CCER_CC4P
+  #define TIM_CCER_CCxE           TIM_CCER_CC4E
+  #define TIM_CCER_CCxP           TIM_CCER_CC4P
 
-#define CCMRx                   CCMR2
-#define TIM_CCMRy_OCxM          TIM_CCMR2_OC4M
-#define TIM_CCMRy_OCxM_2        TIM_CCMR2_OC4M_2
-#define TIM_CCMRy_OCxM_1        TIM_CCMR2_OC4M_1
-#define TIM_CCMRy_OCxPE         TIM_CCMR2_OC4PE
-#define TIM_DIER_CCxDE          TIM_DIER_CC4DE
-#define CCRx                    CCR4
+  #define CCMRx                   CCMR2
+  #define TIM_CCMRy_OCxM          TIM_CCMR2_OC4M
+  #define TIM_CCMRy_OCxM_2        TIM_CCMR2_OC4M_2
+  #define TIM_CCMRy_OCxM_1        TIM_CCMR2_OC4M_1
+  #define TIM_CCMRy_OCxPE         TIM_CCMR2_OC4PE
+  #define TIM_DIER_CCxDE          TIM_DIER_CC4DE
+  #define CCRx                    CCR4
 
-#define DMA1_Channelx           DMA1_Channel7
-#define DMA1_Channelx_IRQn      DMA1_Channel7_IRQn
-#define DMA1_Channelx_IRQHandler        DMA1_Channel7_IRQHandler
+  #define DMA1_Channelx           DMA1_Channel7
+  #define DMA1_Channelx_IRQn      DMA1_Channel7_IRQn
+  #define DMA1_Channelx_IRQHandler        DMA1_Channel7_IRQHandler
 
-#define DMA_CCRx_EN             DMA_CCR7_EN
-#define DMA_CCRx_TCIE           DMA_CCR7_TCIE
+  #define DMA_CCRx_EN             DMA_CCR7_EN
+  #define DMA_CCRx_TCIE           DMA_CCR7_TCIE
 
-#define DMA_IFCR_CTEIFx         DMA_IFCR_CTEIF7
-#define DMA_IFCR_CHTIFx         DMA_IFCR_CHTIF7
-#define DMA_IFCR_CTCIFx         DMA_IFCR_CTCIF7
-#define DMA_IFCR_CGIFx          DMA_IFCR_CGIF7
+  #define DMA_IFCR_CTEIFx         DMA_IFCR_CTEIF7
+  #define DMA_IFCR_CHTIFx         DMA_IFCR_CHTIF7
+  #define DMA_IFCR_CTCIFx         DMA_IFCR_CTCIF7
+  #define DMA_IFCR_CGIFx          DMA_IFCR_CGIF7
 
 #endif
 
@@ -229,7 +229,7 @@ void ws2812b_init(void)
 
 void ws2812b_buff_claer(void)
 {
-  for(int i = 0; i<WS2812B_NUM_LEDS-2; i++)
+  for(int i = 0; i<DATA_LEN-2; i++)
     led_array[i] = WS2812B_0_VAL;
   
   led_array[DATA_LEN-2] = 0;
@@ -289,7 +289,7 @@ int ws2812b_send(void)
     flag_rdy = 0;
     
     //Настраиваем передачу данных
-    DMA1_Channelx->CCR &= ~(DMA_CCR7_EN); //Отключаем канал DMA
+    DMA1_Channelx->CCR &= ~(DMA_CCRx_EN); //Отключаем канал DMA
     DMA1_Channelx->CNDTR = sizeof(led_array); //Устанавливаем количество данных
     
     //Таймер считает до WS2812B_TIMER_AAR, таким образом
